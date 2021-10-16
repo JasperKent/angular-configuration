8b419fbde9cdc08a98793e7a3ce4e74688c75871
Jasper Kent <software@jasperkent.com>
2021-10-16 11:29:38 +0100

    Initial commit

---
+++ src/app/services/book-review-repository.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BookReview } from './book-review';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookReviewRepositoryService {
+
+  private reviews: BookReview[] = [];
+
+  private loadReviews(): void {
+    this.httpClient.get<BookReview[]>('https://codingtutorialsbookreviewapi.azurewebsites.net/BookReview')
+      .subscribe (result => this.reviews = result);
+  }
+
+  constructor (private httpClient: HttpClient){
+    this.loadReviews();
+  }
+
+  getReviews (): BookReview[] {
+    return this.reviews;
+  }
+
+  addReview (review: BookReview): void{
+    this.httpClient.post<any>('https://codingtutorialsbookreviewapi.azurewebsites.net/BookReview', review)
+      .subscribe(result => this.loadReviews());
+  }
+}
+
